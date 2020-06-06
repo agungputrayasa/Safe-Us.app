ec3295ef1f0ef0d22af37b64f8515afaec0adf0f
agungputrayasa <56327546+agungputrayasa@users.noreply.github.com>
2020-06-06 19:08:27 +0700

    FormasiController.php

---
+++ FormasiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormasiController extends Controller
+{
+    public function formasi(){
+        return view('formasi');
+    }
+}
